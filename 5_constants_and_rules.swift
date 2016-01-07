f8e08514663431cecc43c34d8404284dfb306bec
Amit Vaidya <amitvaidhya@gmail.com>
2016-01-07 13:59:25 +0530

    Create 5_constants_and_rules.swift

---
+++ 5_constants_and_rules.swift
@@ -0,0 +1,17 @@
+//constant
+let x = 5
+//naming coonventions.........
+let color = "red"
+//name cannot have spaces
+//let new color = "black"
+//either underscore or camelCase
+let new_color = "black"
+let newColor = "brown"
+
+//disallowed char
+let ab = 12
+//no whitespaces
+//no special char(!,?)
+//no mathematical symbols
+//cannot start with a number...
+
