d9573361f6453f515931d393ea4dbf990391cd10
Amit Vaidya <amitvaidhya@gmail.com>
2016-01-13 10:46:42 +0530

    Create 8_comparison_operator.swift

---
+++ 8_comparison_operator.swift
@@ -0,0 +1,17 @@
+//comparison operator
+
+let str1 = "Apple"
+let str1 = "apple"
+let str1 = "apple"
+
+str1 != str2
+str2 == str3
+
+1>2
+2>1
+
+//ascii value comparison
+
+"a" < "b"
+1 >= 1
+2 <= 2
