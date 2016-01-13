50e8de2502c16edee0d3c5cce510b7ec4e28c5d9
Amit Vaidya <amitvaidhya@gmail.com>
2016-01-13 10:43:32 +0530

    Create 7_binary_operators.swift

---
+++ 7_binary_operators.swift
@@ -0,0 +1,10 @@
+//-------Operators-------
+//binary operators
+//area of rectangle
+let length = 6.0 //in m
+let breadth = 4.0 //in m
+//area in sq. m
+let area = length * breadth
+let areaInSqCm = area * 10000
+print(area)
+print("area in sq. centimetre :",areaInSqCm)
