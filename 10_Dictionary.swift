24a7be6ee2de04734a6c26caa7d571af6a4f92b4
Amit Vaidya <amitvaidhya@gmail.com>
2016-01-21 22:54:31 +0530

    Create 10_Dictionary.swift

---
+++ 10_Dictionary.swift
@@ -0,0 +1,19 @@
+//dictionaries
+
+//hotel db
+
+var hotelDb: [String:String] = [
+    "First" :"Mr.Sam",
+    "Second":"Mr.John",
+    "Third" :"Mr.Tom",
+    "Fourth":"Mrs.Mary"]
+
+//inserting values
+hotelDb["Fifth"] = "Mr.James"
+
+//update key
+hotelDb["Third"] = "Mr.Tomy"
+
+//update or create new
+
+hotelDb.updateValue("Mr.Samy",forKey: "Sixth")
