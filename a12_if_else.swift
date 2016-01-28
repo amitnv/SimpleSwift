84370e6d7b555ccf61d03c9c4ec64e7a71c90b80
Amit Vaidya <amitvaidhya@gmail.com>
2016-01-28 19:05:15 +0530

    Create a12_if_else.swift

---
+++ a12_if_else.swift
@@ -0,0 +1,17 @@
+//conditional statements
+//program to find the greatest number
+
+let a:Int = 8
+let b:Int = 0
+let c:Int = 2
+
+if a>b && a>c {
+    print("a is greatest of all and it's value is \(a)")
+}
+else if b>c {
+    print("b is greatest of all and it's value is \(b)")
+}
+else
+{
+    print("c is greatest of all and it's value is \(c)")
+}
