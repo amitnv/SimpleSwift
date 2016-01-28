90ee3544ba0dfa4ec2dad46c60b82d04db36e4b7
Amit Vaidya <amitvaidhya@gmail.com>
2016-01-28 20:02:14 +0530

    Create a13_switch.swift

---
+++ a13_switch.swift
@@ -0,0 +1,13 @@
+//switch
+
+let weekdays = ["mon","tue","wed","thu","fri"]
+let days = weekdays[4]
+
+switch (days) {
+    case "mon":print("monday")
+    case "tue": print("tuesday")
+    case "wed": print("wednesday")
+    case "thu": print("thurday")
+    default   :print("Friday")
+    break;
+}
